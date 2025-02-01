42574095dff7b583eb7854b0d2eb8df726dac076
keanesworkspace-admin <ngyewsiong@gmail.com>
2025-02-01 14:02:02 +0800

    Level-2

--- src/main/java/Keane.java
+++ src/main/java/Keane.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Keane {
@@ -8,17 +9,29 @@ public class Keane {
 
     public static void main(String[] args) {
         String bye = "bye";
+        String[] words = new String[100];
         System.out.println("Hello! I'm Keane.");
-        System.out.println("What can I do for you?");
+        System.out.println("What is up fella?");
         Scanner in = new Scanner(System.in);
+        int count = 0;
+
+
         while (true){
             String line = in.nextLine();
-
             if (line.equalsIgnoreCase(bye)){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else {
-                System.out.println(line);
+            } else if (line.equalsIgnoreCase("list")){
+                for (String word : words) {
+                    if (word != null) {
+                        System.out.println(word);
+                    }
+                }
+            }
+            else {
+                words[count] = count+1 + ". " + line;
+                count++;
+                System.out.println("Added: "+ line);
             }
         }
         in.close();
