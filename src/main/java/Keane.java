11ff00de7ffb8fd0cf99d47a6a3db2ecae99f3fb
keanesworkspace-admin <ngyewsiong@gmail.com>
2025-02-01 13:28:51 +0800

    Level-1

--- src/main/java/Keane.java
+++ src/main/java/Keane.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
+
 public class Keane {
+
+    private void goodbye(String[] arg){
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
+        String bye = "bye";
         System.out.println("Hello! I'm Keane.");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner in = new Scanner(System.in);
+        while (true){
+            String line = in.nextLine();
+
+            if (line.equalsIgnoreCase(bye)){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(line);
+            }
+        }
+        in.close();
     }
 }
