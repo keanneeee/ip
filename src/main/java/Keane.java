9f935a8311e55ea80ff654f1e3bfbd3492c5cb69
keanesworkspace-admin <ngyewsiong@gmail.com>
2025-02-02 14:27:42 +0800

    Level-3

--- src/main/java/Keane.java
+++ src/main/java/Keane.java
@@ -14,22 +14,38 @@ public class Keane {
         System.out.println("What is up fella?");
         Scanner in = new Scanner(System.in);
         int count = 0;
+        Tasks tasks = new Tasks();
 
-
-        while (true){
+        while (true) {
             String line = in.nextLine();
-            if (line.equalsIgnoreCase(bye)){
+            if (line.equalsIgnoreCase(bye)) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (line.equalsIgnoreCase("list")){
+            } else if (line.equalsIgnoreCase("list")) {
                 for (String word : words) {
                     if (word != null) {
                         System.out.println(word);
                     }
                 }
+            } else if (line.contains("mark") && (Integer.parseInt(line.substring(5)) <= count + 1)) {
+                int taskNum = Integer.parseInt(line.substring(5)) - 1;
+                String temp = words[taskNum];
+                words[taskNum] = temp.replaceFirst(" ", "X");
+                System.out.println("sure king");
+                System.out.println(words[taskNum]);
+                System.out.println("anything else?");
+            } else if(line.contains("unmark") && (Integer.parseInt(line.substring(5)) <= count + 1)){
+                int taskNum = Integer.parseInt(line.substring(5)) - 1;
+                String temp = words[taskNum];
+                System.out.println("sure king");
+                words[taskNum] = temp.replaceFirst("X", " ");
+                System.out.println("anything else?");
+            }  else if ((line.contains("mark")||line.contains("unmark")) && ((Integer.parseInt(line.substring(5)) > count) || (Integer.parseInt(line.substring(5)) < 0))) {
+                System.out.println("king please enter a valid number");
             }
             else {
-                words[count] = count+1 + ". " + line;
+                tasks.setId(line);
+                words[count] = count+1 + "." + tasks.getId();
                 count++;
                 System.out.println("Added: "+ line);
             }
@@ -37,3 +53,4 @@ public class Keane {
         in.close();
     }
 }
+//will edit fringe cases by this week psps
---
+++ src/main/java/Tasks.java
@@ -0,0 +1,12 @@
+public class Tasks {
+    private static String id;
+
+    public void setId(String task) {
+        id = "[ ] " + task;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+}
